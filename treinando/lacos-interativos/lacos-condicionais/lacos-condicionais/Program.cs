17594b299d34dd749eea04fc028d11178cc73686
João Paulo Moreira <jpmoreira195@outlook.com>
2023-04-18 19:35:36 -0300

    Aulas de POO e C#

---
+++ treinando/lacos-interativos/lacos-condicionais/lacos-condicionais/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lacosCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Laços condicionais => execução mediante determinada condição/situação satisfeita
+
+            int firstCarSpeed = 150;
+            int secondCarSpeed = 145;
+
+            if (firstCarSpeed >= secondCarSpeed)
+            {
+                Console.WriteLine("O carro 1 está mais rapido que o carro 2");
+            }else
+            {
+                Console.WriteLine("Não consegui identificar qual carro está mais rápido");
+            }
+
+        }
+    }
+}
